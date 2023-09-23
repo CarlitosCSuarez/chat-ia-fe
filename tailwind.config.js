b961d62a19f7d59199161de4cc86e482c998f5f3
Carlos Calpa <iamcarlosacs@gmail.com>
2023-09-23 15:46:03 -0500

    First commit.

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    container: {
+      center: true
+    },
+    fontSize: {
+      base: '1rem',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
